9417cba9d67dfe10d03f0a666976d196d91ad5a6
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-26 17:41:23 +0600

    adding navber done

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -3,6 +3,8 @@ import logo from '../../../assets/logo.png'
 import moment from 'moment';
 import { Button, Container } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
     return (
@@ -21,7 +23,26 @@ const Header = () => {
                 I can be a React component, multiple React components, or just some text......
                 </Marquee>
             </div>
-        </Container>
+
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mx-auto">
+                    <Nav.Link href="#Home">Home</Nav.Link>
+                    <Nav.Link href="#About">About</Nav.Link>
+                    <Nav.Link href="#Career">Career</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="#deets">Profile</Nav.Link>
+                    <Nav.Link eventKey={2} href="#memes">
+                    <Button variant="secondary">Login</Button>
+                    </Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+            </Container>
     );
 };
 
