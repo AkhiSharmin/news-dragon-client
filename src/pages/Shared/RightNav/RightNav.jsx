8551d4e363ce7eba250c1103b5d06b705371d846
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-26 18:55:20 +0600

    right nav icon done

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebookF, FaTwitter, FaInstagram  } from 'react-icons/fa';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right Nav</h2>
+            <h2>Login With</h2>
+            <Button className='mb-2' variant="outline-secondary"> <FaGoogle /> Login with Google</Button>
+            <Button variant="outline-success"> <FaGithub /> Login with Github</Button>
+            <div>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item> <FaFacebookF></FaFacebookF> FaceBook
+                    </ListGroup.Item>
+                    <ListGroup.Item> <FaTwitter></FaTwitter>
+                        Twitter
+                    </ListGroup.Item>
+                    <ListGroup.Item> <FaInstagram></FaInstagram>
+                        Instagram 
+                    </ListGroup.Item>
+                    
+                </ListGroup>
+            </div>
         </div>
     );
 };
