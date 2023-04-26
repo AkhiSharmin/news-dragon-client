e469527bac24043ee61dceae502716afcf268246
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-27 03:41:17 +0600

    adding newscard

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams, useLoaderData } from 'react-router-dom';
+import NewsCard from '../NewsCard/NewsCard';
 
 const Category = () => {
 
     const {id} = useParams();
+    const categoryNews = useLoaderData();
 
     return (
         <div>
-            <h2>This is a Category: {id}</h2>
+            <h2>This Category News: {categoryNews.length}</h2>
+            {
+                categoryNews.map(news => <NewsCard
+                key={news_id}
+                news={news}
+                ></NewsCard>)
+            }
         </div>
     );
 };
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Category from "../pages/Home/Category/Category";
+import NewsLayout from "../layout/NewsLayout";
+import News from "../pages/News/News/News";
 
 
 const router = createBrowserRouter([
@@ -15,9 +17,20 @@ const router = createBrowserRouter([
             },
             {
                 path: '/category/:id',
-                element: <Category></Category>
+                element: <Category></Category>,
+                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
             }
         ]
+    },
+    {
+       path: '/news',
+       element:  <NewsLayout></NewsLayout>,
+       children: [
+            {
+                path: ':id',
+                element: <News></News>
+            }
+       ]
     }
 ])
 
