5f9e6583cf69600f9c928c67f5c7e76527fe1889
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-27 05:40:55 +0600

    added icon

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,14 +1,26 @@
+import moment from 'moment';
 import React from 'react';
+import { Image } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import { FaEye, FaRegBookmark, FaShareAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const NewsCard = ({news}) => {
 
-    const {_id, title, details, image_url } = news;
+    const {_id, title, details, image_url, author, total_view, rating } = news;
 
     return (
         <Card className="mb-4">
-      <Card.Header>Featured</Card.Header>
+      <Card.Header className='d-flex align-items-center'>
+      <Image style={{height: '40px'}} src={author?.img} roundedCircle />
+      <div className='ps-2 flex-grow-1'>
+            <p className='mb-0'>{author?.name}</p>
+            <p><small> {moment(author?.published_date).format('yyyy-MM-D') }</small></p>
+      </div>
+      <div>
+         <FaRegBookmark></FaRegBookmark> <FaShareAlt></FaShareAlt>
+      </div>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={image_url} />
@@ -16,7 +28,14 @@ const NewsCard = ({news}) => {
         <>{details.slice(0,250)}... <Link to={`/news/${_id}`}>Read More</Link> </>
         }</Card.Text>
       </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      <Card.Footer className="text-muted d-flex">
+           <div className='flex-grow-1'>
+                <span>{rating?.number}</span>
+           </div>
+           <div>
+               <FaEye></FaEye> {total_view} 
+            </div> 
+      </Card.Footer>
     </Card>
     );
 };
