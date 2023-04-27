ad777fafb112d001bd09c888a2e215bb74076bf6
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-28 01:35:22 +0600

    editorLnsight added

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
-import { useLoaderData } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link ,  useLoaderData } from 'react-router-dom';
 
 const News = () => {
     const news = useLoaderData();
-    const {_id, title, details, image_url, author, total_view, rating } = news;
+    const {_id, title, details, image_url, category_id } = news;
     return (
         <Card>
         <Card.Img variant="top" src={image_url} />
@@ -13,7 +14,7 @@ const News = () => {
           <Card.Text>
                 {details}
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Link to={`/category/${category_id}`}><Button variant="danger"> <FaArrowLeft></FaArrowLeft> All News in this Category!!</Button></Link>
         </Card.Body>
       </Card>
     );
