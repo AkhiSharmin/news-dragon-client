fd617632744f15a27f9002a4a25da1ed14d18382
AkhiSharmin <akhisharmin100@gmail.com>
2023-04-28 16:34:43 +0600

    register page design done

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,20 +12,37 @@ const Login = () => {
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" name='email' placeholder="Enter email" required/>
                 
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" name='password' placeholder="Password" required/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
+            
             <Button variant="primary" type="submit">
-                Submit
+                Login
             </Button>
+            <br/>
+            <Form.Text className='text-success'>
+
+            </Form.Text>
+
+            <Form.Text className='text-secandry'>
+            Don't have an Account?<Link to='/register'>Register</Link>
+            </Form.Text>
+
+            <Form.Text className='text-success'>
+
+            </Form.Text>
+
+            <Form.Text className='text-danger'>
+
+            </Form.Text>
         </Form>
         </Container>
     );
